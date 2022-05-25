1736ee31620a180c8d35c833b260c70e8977d17a
Mflores311 <104598685+Mflores311@users.noreply.github.com>
2022-05-25 16:14:50 -0500

    Add files via upload

---
+++ final.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Heroku</title>
+
+    <link rel="stylesheet" href="css/estilosfinal.css">
+    <link rel="stylesheet" href="css/diseñoresponsivo.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,600,700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <!-- Header -->
+    <header id="header">
+     <div class="img-header">
+         <div id="particles-js">
+         </div>
+         <div class="header-info">
+             <h2>HEROKU</h2>
+             <hr>
+             <p>Servidor en línea</p>
+         </div>
+     </div>
+
+     <!-- Navegation Menu -->
+     <nav class="menu">
+         <div class="log-box">
+             <h1>Información</h1>
+             <span class="btn-menu"><i class="fas fa-bars"></i></span>
+         </div>
+
+         <div class="list-container">
+             <ul class="lists">
+                 <li><a href="#" class="active">Inicio</a></li>
+                 <li><a href="#">Trabajos</a></li>
+                 <li><a href="#">Formación</a></li>
+                 <li><a href="#">Acerca De</a></li>
+                 <li><a href="#">Contacto</a></li>
+             </ul>
+         </div>
+     </nav>
+
+    </header>
+
+<main id="main">
+   
+    <!-- Mission -->
+    <section class="mission">
+        <div class="mission-info">
+            <h2>Misión</h2>
+            <p> Ser un ingeniero dinámico, que responda a los requerimientos locales, nacionales y globales, que tome en consideración los avances y tendencias de desarrollo tecnológico, que posibilite la formación académica y personal de profesionistas capaces y comprometidos.</p>
+            <p>La visión sistémica nos permite conocer qué principios o leyes ayudan a que las relaciones mejoren, entre los miembros de un sistema, en cuanto a fluidez, transparencia y eficacia.</p>
+        </div>
+        <div class="spacey-img">
+            <img src="images/spacex.jpg">
+        </div>
+    </section>
+
+    <!-- Parallax Elon Musk -->
+    <section class="elon-musk">
+      <div class="skew-arriba"></div>
+        <figure class="figure-img">
+           <h4>"La ingeniería es lo más cercano a la magia que existe en el mundo."-Elon Musk-</h4>
+        </figure>
+      <div class="skew-abajo"></div>
+    </section>
+
+    <!-- Img Gallery -->
+    <section class="gallery-section">
+        <div class="gallery-container">
+            <div class="img-card"><img src="images/lanzamiento-1.jpg" alt="lanzamiento-1"></div>
+            <div class="img-card"><img src="images/lanzamiento-2.jpg" alt="lanzamiento-2"></div>
+            <div class="img-card"><img src="images/lanzamiento-3.jpg" alt="lanzamiento-3"></div>
+            <div class="img-card"><img src="images/lanzamiento-4.jpg" alt="lanzamiento-4"></div>
+            <div class="img-card"><img src="images/lanzamiento-5.jpeg" alt="lanzamiento-5"></div>
+            <div class="img-card"><img src="images/lanzamiento-6.jpg" alt="lanzamiento-6"></div>
+            <div class="img-card"><img src="images/lanzamiento-7.jpg" alt="lanzamiento-7"></div>
+            <div class="img-card"><img src="images/lanzamiento-8.jpg" alt="lanzamiento-8"></div>
+            <div class="img-card"><img src="images/lanzamiento-9.jpg" alt="lanzamiento-9"></div>
+        </div>
+        
+        <div class="modal-gallery">
+          <span class="close-modal"><i class="fas fa-times"></i></span>
+
+          <img src="" id="imgModal">
+          
+          <div class="captionModal"></div>
+
+          <div class="gallery-list">
+                <img src="images/lanzamiento-1.jpg" alt="lanzamiento-1">
+                <img src="images/lanzamiento-2.jpg" alt="lanzamiento-2">
+                <img src="images/lanzamiento-3.jpg" alt="lanzamiento-3">
+                <img src="images/lanzamiento-4.jpg" alt="lanzamiento-4">
+                <img src="images/lanzamiento-5.jpeg" alt="lanzamiento-5">
+                <img src="images/lanzamiento-6.jpg" alt="lanzamiento-6">
+                <img src="images/lanzamiento-7.jpg" alt="lanzamiento-7">
+                <img src="images/lanzamiento-8.jpg" alt="lanzamiento-8">
+                <img src="images/lanzamiento-9.jpg" alt="lanzamiento-9">
+          </div>
+        </div>
+
+    </section>
+</main>
+
+<!-- Go Top -->
+<div class="go-top">
+    <span><i class="fas fa-chevron-up"></i></span>
+</div>
+
+<!-- Footer -->
+<footer id="footer">
+    <p>&copy; Miguel Flores Derechos Reservados 2021</p>
+</footer>
+
+<!-- Scripts -->
+<script src="https://kit.fontawesome.com/35db202371.js" crossorigin="anonymous"></script>
+<script src="js/particles.js"></script>
+<script src="js/particles-lib.js"></script>
+<script src="js/app.js"></script>
+
+</body>
+</html>
